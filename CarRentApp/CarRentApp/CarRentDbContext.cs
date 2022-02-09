a1a3fce90a591e82bfb24ddfeb289e8620d76b96
TheCodeFather13 <maman_66@mail.ru>
2022-02-09 18:28:49 +0200

    Add DbContext

---
+++ CarRentApp/CarRentApp/CarRentDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CarRentApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentApp
+{
+    public class CarRentDbContext : DbContext
+    {
+        public CarRentDbContext() : base("MyConnectionString")
+        {
+
+        }
+        public virtual DbSet<CarRentalRecord> CarRentalRecords { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<TypesOfCar> TypesOfCars { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserRole> UserRoles { get; set; }
+
+
+    }
+}
